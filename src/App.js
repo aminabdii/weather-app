ef012d67f644cd3f4414873325236916aa1fff10
amin abdi <amin.s2254@gmail.com>
2022-05-12 03:48:09 +0430

    components styel completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
+
 
 function App() {
+
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=amol&appid=612fe3536c632e827fd6e6904793c05a'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+       {/* TOP SIDE */}
+        <div className='top'>
+
+          <div className='location'>
+            <p>amol</p>
+          </div>
+          <div className='temp'>
+            <h1>12c</h1>
+          </div>
+          <div className='description'>
+            <p>Rainy</p>
+          </div>
+    
+        </div>
+        {/* RIGHT SIDE */}
+
+        <div className='bottom'>
+
+          <div className='feels'>
+            <p className='bold'>12c</p>
+            <p>Feels Like</p>
+          </div>
+          <div className='humidity'>
+            <p className='bold'>20%</p>
+            <p>Humiditys</p>
+          </div>
+          <div className='wind'>
+          <p className='bold'>12 MPH</p>
+          <p>Wind speed</p>
+          </div>
+
+        </div>
+      </div>
+
+      
     </div>
   );
 }
